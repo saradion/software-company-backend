1decc1745ca7dc951189d626883e8cbb7a52a950
Sara <141394625+saradion@users.noreply.github.com>
2026-07-18 17:47:32 +0330

    Update wsgi.py

--- sst/wsgi.py
+++ sst/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sst_website.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sst.settings')
 
 application = get_wsgi_application()
